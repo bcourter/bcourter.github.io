19587d7b91e1934dacfb6087384f5470c92ad15e
Blake Courter <bcourter@gmail.com>
2024-03-24 15:23:47 -0400

    ForceGraph working with links

--- landscape/main.js
+++ landscape/main.js
@@ -1,26 +1,55 @@
-import BubbleChart from "./BubbleChart.js";
+import ForceGraph from "./ForceGraph.js";
 
 const url =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vQqpp3EgZEXoDrQ8MVxr7AS2ifn3Efvlk7QFEEe9Z1iMtOXX7QX5ZK5XQg0EcrSun1umA0-nCr5BQFR/pub?gid=595724753&single=true&output=tsv";
 
-const components = [
-  "AI_ML",
-  "Generative",
-  "MCAD",
-  "CAE",
-  "CFD",
-  "CAM_MES",
-  "EDA",
-  "AEC",
-  "IM_PM",
-  "PDM_PLM",
-  "VnV_SCM",
-  "Hardware",
-  "Ecosystem_Community",
-  "B_rep",
-  "Implicit",
-  "Physics",
+let enduserColor = d3.hcl(0, 22, 88);
+let componentColor = d3.hcl(0, 22, 66);
+
+const hues = [
+  "red",
+  "orange",
+  "yellow",
+  "olive",
+  "green",
+  "teal",
+  "blue",
+  "cyan",
+  "azure",
+  "blue",
+  "violet",
+  "magenta",
+  "rose",
 ];
+
+function hueOf(name) {
+  return hues.indexOf(name) * 30;
+}
+
+function setHue(hcl, hue) {
+  let op = hcl.copy();
+  op.h = hue;
+  return op;
+}
+
+const components = {
+  AI_ML: d3.hcl(0, 44, 88),
+  Generative: d3.hcl(0, 33, 88),
+  MCAD: setHue(enduserColor, hueOf("yellow")),
+  CAE: setHue(enduserColor, hueOf("blue")),
+  CFD: setHue(enduserColor, hueOf("violet")),
+  CAM_MES: setHue(enduserColor, hueOf("red")),
+  EDA: setHue(enduserColor, hueOf("green")),
+  AEC: setHue(enduserColor, hueOf("rose")),
+  IM_PM: setHue(enduserColor, hueOf("olive")),
+  PDM_PLM: d3.hcl(0, 0, 66),
+  VnV_SCM: setHue(enduserColor, hueOf("orange")),
+  Hardware: d3.hcl(0, 0, 88),
+  Ecosystem_Community: d3.hcl(0, 0, 66),
+  B_rep: setHue(componentColor, hueOf("yellow")),
+  Implicit: setHue(componentColor, hueOf("cyan")),
+  Physics: setHue(componentColor, hueOf("blue")),
+};
 
 const stageToSize = {
   "Tightly Held": 5,
@@ -37,25 +66,28 @@ const stageToSize = {
 let brandValue = 2.0;
 const brandMap = new Object();
 
-function VendorDot(a, b) {
+function vendorDot(a, b) {
   let sum = 0;
-  components.forEach((c) => {
+  Object.keys(components).forEach((c) => {
     sum += a[c] * b[c];
   });
   return sum;
 }
 
-function ProcessVendor(d) {
+function processVendor(d) {
+  d.id = d.Company;
   d.magnitude = 0;
-  components.forEach((c) => {
-    if (!d[c]) {
-      d[c] = 0;
-      return;
+  d.color = d3.rgb(0, 0, 0);
+  let colorAverageCount = 0;
+  for (const [key, hcl] of Object.entries(components)) {
+    if (!d[key]) {
+      d[key] = 0;
+      continue;
     }
 
-    let num = Number(d[c]);
+    let num = Number(d[key]);
     if (isNaN(num)) {
-      let brands = d[c].split(",");
+      let brands = d[key].split(",");
       brands.forEach((s) => {
         brandMap[s.trim()] = d;
       });
@@ -63,7 +95,19 @@ function ProcessVendor(d) {
     }
 
     d.magnitude += num;
-  });
+
+    let rgb = d3.rgb(hcl);
+    d.color.r += rgb.r;
+    d.color.g += rgb.g;
+    d.color.b += rgb.b;
+    colorAverageCount++;
+  }
+
+  if (colorAverageCount != 0) {
+    d.color.r /= colorAverageCount;
+    d.color.g /= colorAverageCount;
+    d.color.b /= colorAverageCount;
+  }
 
   d.stageSize = stageToSize[d.Stage];
   if (typeof d.stageSize != "number") d.stageSize = 5;
@@ -72,17 +116,46 @@ function ProcessVendor(d) {
   return d;
 }
 
-function Initialize(data) {
-  let chart = BubbleChart(data, {
-    label: (d) => d.Company.split(" ").join("\n"),
-    value: (d) => d.size,
-    // group: (d) => d.id.split(".")[1],
-    title: (d) => d.Company,
-    link: (d) => d.url,
-    width: 1452,
-  });
+function initialize(data) {
+  //   let chart = BubbleChart(data, {
+  //     label: (d) => d.Company.split(" ").join("\n"),
+  //     value: (d) => d.size,
+  //     // group: (d) => d.id.split(".")[1],
+  //     title: (d) => d.Company,
+  //     link: (d) => d.url,
+  //     width: 1452,
+  //   });
+
+  data = data.filter((v) => v.magnitude > 0);
+  const links = [];
+
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < i; j++) {
+      let dot = vendorDot(data[i], data[j]);
+      if (dot > 0)
+        links.push({
+          source: data[i].Company,
+          target: data[j].Company,
+          value: dot,
+        });
+    }
+  }
+
+  let chart = ForceGraph(
+    { nodes: data, links: links },
+    {
+      nodeID: (d) => d.Company,
+      nodeTitle: (d) => d.Company,
+      nodeRadius: (d) => d.size,
+      nodeFill: (d) => (d ? d.color.formatHex() : "#fff"),
+      //   link: (d) => d.url,
+      linkStrength: 0.01,
+      width: 1452,
+      height: 1024,
+    }
+  );
 
   container.append(chart);
 }
 
-d3.tsv(url, ProcessVendor).then(Initialize);
+d3.tsv(url, processVendor).then(initialize);
