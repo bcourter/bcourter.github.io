eb58d27483db5af5ed6630a3ed1d7a7bbbf174b0
Blake Courter <bcourter@gmail.com>
2024-03-24 23:48:33 -0400

    Force layout, color, and labeling

--- landscape/main.js
+++ landscape/main.js
@@ -3,8 +3,8 @@ import ForceGraph from "./ForceGraph.js";
 const url =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vQqpp3EgZEXoDrQ8MVxr7AS2ifn3Efvlk7QFEEe9Z1iMtOXX7QX5ZK5XQg0EcrSun1umA0-nCr5BQFR/pub?gid=595724753&single=true&output=tsv";
 
-let enduserColor = d3.hcl(0, 22, 88);
-let componentColor = d3.hcl(0, 22, 66);
+let enduserHCL = d3.hcl(0, 22, 88);
+let componentHCL = d3.hcl(0, 33, 66);
 
 const hues = [
   "red",
@@ -13,7 +13,6 @@ const hues = [
   "olive",
   "green",
   "teal",
-  "blue",
   "cyan",
   "azure",
   "blue",
@@ -32,24 +31,30 @@ function setHue(hcl, hue) {
   return op;
 }
 
-const components = {
-  AI_ML: d3.hcl(0, 44, 88),
-  Generative: d3.hcl(0, 33, 88),
-  MCAD: setHue(enduserColor, hueOf("yellow")),
-  CAE: setHue(enduserColor, hueOf("blue")),
-  CFD: setHue(enduserColor, hueOf("violet")),
-  CAM_MES: setHue(enduserColor, hueOf("red")),
-  EDA: setHue(enduserColor, hueOf("green")),
-  AEC: setHue(enduserColor, hueOf("rose")),
-  IM_PM: setHue(enduserColor, hueOf("olive")),
-  PDM_PLM: d3.hcl(0, 0, 66),
-  VnV_SCM: setHue(enduserColor, hueOf("orange")),
-  Hardware: d3.hcl(0, 0, 88),
-  Ecosystem_Community: d3.hcl(0, 0, 66),
-  B_rep: setHue(componentColor, hueOf("yellow")),
-  Implicit: setHue(componentColor, hueOf("cyan")),
-  Physics: setHue(componentColor, hueOf("blue")),
+const industries = {
+  CAM_MES: setHue(enduserHCL, hueOf("orange")),
+  MCAD: setHue(enduserHCL, hueOf("olive")),
+  CAE: setHue(enduserHCL, hueOf("teal")),
+  CFD: setHue(enduserHCL, hueOf("cyan")),
+  EDA: setHue(enduserHCL, hueOf("azure")),
+  IM_PM: setHue(enduserHCL, hueOf("violet")),
+  AEC: setHue(enduserHCL, hueOf("rose")),
+
+  B_rep: setHue(componentHCL, hueOf("yellow")),
+  Implicit: setHue(componentHCL, hueOf("green")),
+  Physics: setHue(componentHCL, hueOf("teal")),
 };
+
+const qualities = {
+  AI_ML: d3.hcl(0, 6, 0),
+  Generative: d3.hcl(0, 4, 0),
+  PDM_PLM: d3.hcl(0, 0, -4),
+  VnV_SCM: d3.hcl(0, 0, -4),
+  Hardware: d3.hcl(0, -11, 0),
+  Ecosystem_Community: d3.hcl(0, 11, -4),
+};
+
+const allcategories = { ...industries, ...qualities };
 
 const stageToSize = {
   "Tightly Held": 5,
@@ -68,94 +73,226 @@ const brandMap = new Object();
 
 function vendorDot(a, b) {
   let sum = 0;
-  Object.keys(components).forEach((c) => {
+  Object.keys(allcategories).forEach((c) => {
     sum += a[c] * b[c];
   });
   return sum;
 }
 
+function getNumber(d, key) {
+  if (!d[key]) {
+    d[key] = 0;
+    return 0;
+  }
+
+  let num = Number(d[key]);
+  if (isNaN(num)) {
+    let brands = d[key].split(",");
+    brands.forEach((s) => {
+      brandMap[s.trim()] = d;
+    });
+    num = brandValue * brands.length;
+    d[key] = num;
+  }
+
+  return num;
+}
+
 function processVendor(d) {
   d.id = d.Company;
+
+  if (d.Company == "Siemens PLM") {
+    let hi = 0;
+  }
+
   d.magnitude = 0;
   d.color = d3.rgb(0, 0, 0);
-  let colorAverageCount = 0;
-  for (const [key, hcl] of Object.entries(components)) {
-    if (!d[key]) {
-      d[key] = 0;
-      continue;
-    }
-
-    let num = Number(d[key]);
-    if (isNaN(num)) {
-      let brands = d[key].split(",");
-      brands.forEach((s) => {
-        brandMap[s.trim()] = d;
-      });
-      num = brandValue * brands.length;
-    }
-
+  for (const [key, hclValue] of Object.entries(industries)) {
+    const num = getNumber(d, key);
     d.magnitude += num;
 
-    let rgb = d3.rgb(hcl);
-    d.color.r += rgb.r;
-    d.color.g += rgb.g;
-    d.color.b += rgb.b;
-    colorAverageCount++;
+    let rgb = d3.rgb(hclValue);
+    d.color.r += rgb.r * num;
+    d.color.g += rgb.g * num;
+    d.color.b += rgb.b * num;
   }
 
-  if (colorAverageCount != 0) {
-    d.color.r /= colorAverageCount;
-    d.color.g /= colorAverageCount;
-    d.color.b /= colorAverageCount;
+  if (d.magnitude != 0) {
+    d.color.r /= d.magnitude;
+    d.color.g /= d.magnitude;
+    d.color.b /= d.magnitude;
   }
+
+  let hcl = d3.hcl(d.color);
+  if (!hcl.h || !hcl.c) hcl = enduserHCL;
+
+  let qualitiesSum = 0;
+  for (const [key, hclValue] of Object.entries(qualities)) {
+    const num = getNumber(d, key);
+    d.magnitude += num;
+    qualitiesSum += num;
+
+    hcl.c += hclValue.c * num;
+    hcl.l += hclValue.l * num;
+  }
+
+  d.color = d3.color(hcl);
+
+  const angle = (hcl.h * Math.PI) / 180;
+  d.dx = Math.cos(angle);
+  d.dy = -Math.sin(angle);
+  d.special = Math.min(1, hcl.c / 10);
 
   d.stageSize = stageToSize[d.Stage];
   if (typeof d.stageSize != "number") d.stageSize = 5;
 
-  d.size = d.magnitude + d.stageSize;
+  d.size = Math.sqrt(2.5 * d.magnitude + d.stageSize);
   return d;
 }
 
-function initialize(data) {
-  //   let chart = BubbleChart(data, {
-  //     label: (d) => d.Company.split(" ").join("\n"),
-  //     value: (d) => d.size,
-  //     // group: (d) => d.id.split(".")[1],
-  //     title: (d) => d.Company,
-  //     link: (d) => d.url,
-  //     width: 1452,
-  //   });
-
-  data = data.filter((v) => v.magnitude > 0);
+function initialize(nodes) {
+  nodes = nodes.filter((v) => v.magnitude > 0);
   const links = [];
 
-  for (let i = 0; i < data.length; i++) {
-    for (let j = 0; j < i; j++) {
-      let dot = vendorDot(data[i], data[j]);
-      if (dot > 0)
-        links.push({
-          source: data[i].Company,
-          target: data[j].Company,
-          value: dot,
-        });
-    }
-  }
+  //   for (let i = 0; i < nodes.length; i++) {
+  //     for (let j = 0; j < i; j++) {
+  //       let dot = vendorDot(nodes[i], nodes[j]);
+  //       if (dot > 0)
+  //         links.push({
+  //           source: nodes[i].Company,
+  //           target: nodes[j].Company,
+  //           value: dot,
+  //         });
+  //     }
+  //   }
 
-  let chart = ForceGraph(
-    { nodes: data, links: links },
-    {
-      nodeID: (d) => d.Company,
-      nodeTitle: (d) => d.Company,
-      nodeRadius: (d) => d.size,
-      nodeFill: (d) => (d ? d.color.formatHex() : "#fff"),
-      //   link: (d) => d.url,
-      linkStrength: 0.01,
-      width: 1452,
-      height: 1024,
-    }
-  );
+  let chart = function () {
+    // Specify the dimensions of the chart.
+    const width = container.clientWidth;
+    const height = container.clientHeight;
+    const size = Math.min(width, height);
+    const explodeRadius = size * 0.2;
+    const lineThickness = 1.5;
 
-  container.append(chart);
+    function getRadius(d) {
+      return (d.size * size) / 160;
+    }
+
+    // Create a simulation with several forces.
+    const simulation = d3
+      .forceSimulation(nodes)
+      .force(
+        "link",
+        d3.forceLink(links).id((d) => d.id)
+      )
+      .force("charge", d3.forceManyBody())
+      .force(
+        "collide",
+        d3.forceCollide((d) => getRadius(d) + lineThickness)
+      )
+      .force(
+        "x",
+        d3.forceX().x((d) => d.dx * d.special * explodeRadius)
+      )
+      .force(
+        "y",
+        d3.forceY().y((d) => d.dy * d.special * explodeRadius)
+      );
+
+    // Create the SVG container.
+    const svg = d3
+      .create("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("viewBox", [-width / 2, -height / 2, width, height])
+      .attr("style", "max-width: 100%; height: auto;");
+
+    // Add a line for each link, and a circle for each node.
+    const link = svg
+      .append("g")
+      .attr("stroke", "#999")
+      .attr("stroke-opacity", 0.6)
+      .selectAll("line")
+      .data(links)
+      .join("line")
+      .attr("stroke-width", (d) => Math.sqrt(d.value));
+
+    const node = svg
+      .append("g")
+      .attr("stroke-width", lineThickness)
+      .selectAll("circle")
+      .data(nodes)
+      .join("g");
+
+    node
+      .append("circle")
+      .attr("r", (d) => getRadius(d))
+      .attr("fill", (d) => d.color)
+      .attr("stroke", (d) => d.color.darker(1));
+
+    node.append("title").text((d) => d.Company);
+
+    // Add a label.
+    const text = node
+      .append("text")
+      .attr("fill-opacity", 0.7)
+      .attr("fill", "#000")
+      .attr("text-anchor", "middle")
+      .attr("style", "label")
+      .attr("clip-path", (d) => `circle(${getRadius(d) - lineThickness})`)
+      .text((d) => d.Company);
+
+    // Add a drag behavior.
+    node.call(
+      d3
+        .drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended)
+    );
+
+    // Set the position attributes of links and nodes each time the simulation ticks.
+    simulation.on("tick", () => {
+      link
+        .attr("x1", (d) => d.source.x)
+        .attr("y1", (d) => d.source.y)
+        .attr("x2", (d) => d.target.x)
+        .attr("y2", (d) => d.target.y);
+
+      //   node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+      node.attr("transform", (d) => `translate(${d.x},${d.y})`);
+    });
+
+    // Reheat the simulation when drag starts, and fix the subject position.
+    function dragstarted(event) {
+      if (!event.active) simulation.alphaTarget(0.3).restart();
+      event.subject.fx = event.subject.x;
+      event.subject.fy = event.subject.y;
+    }
+
+    // Update the subject (dragged node) position during drag.
+    function dragged(event) {
+      event.subject.fx = event.x;
+      event.subject.fy = event.y;
+    }
+
+    // Restore the target alpha so the simulation cools after dragging ends.
+    // Unfix the subject position now that it’s no longer being dragged.
+    function dragended(event) {
+      if (!event.active) simulation.alphaTarget(0);
+      event.subject.fx = null;
+      event.subject.fy = null;
+    }
+
+    // When this cell is re-run, stop the previous simulation. (This doesn’t
+    // really matter since the target alpha is zero and the simulation will
+    // stop naturally, but it’s a good practice.)
+    // invalidation.then(() => simulation.stop());
+
+    return svg.node();
+  };
+
+  container.append(chart());
 }
 
 d3.tsv(url, processVendor).then(initialize);
